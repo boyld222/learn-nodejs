f7669dcc52e334d57b609a89459d0f0615744018
kiet.le <boyld222@gmail.com>
2025-12-02 14:03:02 +0700

    update api

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,25 @@
 import User from "../models/User.js";
 
-
 export const getAllUser = (req, res) => {
     User.find()
         .then((users) => res.json(users))
         .catch((err) => res.status(400).json("Error: " + err));
-}
+};
+
+export const getUserById = (req, res) => {
+    const userId = req.params.id;
+    
+    User.findById(userId)
+        .then((user) => {
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            res.json(user);
+        })
+        .catch((err) => {
+            if (err.name === 'CastError') {
+                return res.status(400).json({ message: 'Invalid user ID format' });
+            }
+            res.status(500).json({ message: 'Error fetching user', error: err.message });
+        });
+};
